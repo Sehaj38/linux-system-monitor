d9a4f62564eda56e4f779a58979d0758ef8ab44c
Sehaj <kaursehajneet06@gmail.com>
2026-01-03 22:46:29 +0530

    Parse CPU timing values from /proc/stat

--- main.cpp
+++ main.cpp
@@ -1,6 +1,20 @@
 #include <iostream>
+#include <fstream>
+#include <string>
+#include<sstream>
+using namespace std;
 
 int main() {
-    std::cout << "System Monitor Starting...\n";
+    ifstream file("/proc/stat");
+    string cpu;
+    long user, nice, system, idle;
+
+    if(file.is_open()){
+        file >> cpu >> user >> nice >> system >> idle;
+        cout << "User : " << user << endl;
+        cout << "System : " << system << endl;
+        cout << "Idle : " << idle << endl;
+    }
+
     return 0;
 }
